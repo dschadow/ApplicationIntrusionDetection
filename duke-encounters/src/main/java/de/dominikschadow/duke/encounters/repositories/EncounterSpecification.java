0a72264a071bb7a33d3132f161a1bbe71cbb0275
Dominik Schadow <dominikschadow@googlemail.com>
2015-08-20 21:17:21 +0200

    Lambdas, moved AIDs to service

--- duke-encounters/src/main/java/de/dominikschadow/duke/encounters/repositories/EncounterSpecification.java
+++ duke-encounters/src/main/java/de/dominikschadow/duke/encounters/repositories/EncounterSpecification.java
@@ -1,24 +1,36 @@
 package de.dominikschadow.duke.encounters.repositories;
 
 import de.dominikschadow.duke.encounters.domain.Encounter;
-import de.dominikschadow.duke.encounters.domain.SearchFilter;
+import de.dominikschadow.duke.encounters.domain.Likelihood;
 import org.springframework.data.jpa.domain.Specification;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 
 /**
  * @author Dominik Schadow
  */
 public class EncounterSpecification {
-    public static Specification<Encounter> findByFilter(SearchFilter filter) {
-        return new Specification<Encounter>() {
-            public Predicate toPredicate(Root<Encounter> root, CriteriaQuery<?> query,
-                                         CriteriaBuilder cb) {
-                return cb.equal(root.<String>get("country"), filter.getCountry());
-            }
-        };
+    public static Specification<Encounter> encounterAfterYear(int year) {
+        // TODO date contains timestamp
+        return (root, query, cb) -> cb.greaterThanOrEqualTo(root.<Integer>get("date"), year);
+    }
+
+    public static Specification<Encounter> encounterByConfirmations(int confirmations) {
+        // TODO count confirmations
+        return (root, query, cb) -> cb.greaterThanOrEqualTo(root.<Integer>get(""), confirmations);
+    }
+
+    public static Specification<Encounter> encounterByEvent(String event) {
+        return (root, query, cb) -> cb.equal(root.<String>get("event"), event);
+    }
+
+    public static Specification<Encounter> encounterByLocation(String location) {
+        return (root, query, cb) -> cb.equal(root.<String>get("location"), location);
+    }
+
+    public static Specification<Encounter> encounterByCountry(String country) {
+        return (root, query, cb) -> cb.equal(root.<String>get("country"), country);
+    }
+
+    public static Specification<Encounter> encounterByLikelihood(Likelihood likelihood) {
+        return (root, query, cb) -> cb.equal(root.<String>get("likelihood"), likelihood.getName());
     }
 }
