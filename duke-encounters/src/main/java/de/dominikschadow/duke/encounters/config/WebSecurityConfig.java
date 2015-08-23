78ba3e463e3e7d6bd7a8f43e3999cbbe2e5cc3d1
Dominik Schadow <dominikschadow@googlemail.com>
2015-08-23 13:32:26 +0200

    Added WebSecurity config

---
+++ duke-encounters/src/main/java/de/dominikschadow/duke/encounters/config/WebSecurityConfig.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2015 Dominik Schadow, dominikschadow@gmail.com
+ *
+ * This file is part of the Application Intrusion Detection project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.dominikschadow.duke.encounters.config;
+
+import org.owasp.appsensor.integration.springsecurity.context.AppSensorSecurityContextRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.web.context.SecurityContextRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+/**
+ * Spring Security configuration file.
+ *
+ * @author Dominik Schadow
+ */
+@Configuration
+@EnableWebMvcSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private SecurityContextRepository securityContextRepository;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/", "/webjars/bootstrap/**", "/img/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .securityContext()
+                .securityContextRepository(securityContextRepository)
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login")
+                .invalidateHttpSession(true)
+                .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+    }
+
+    @Bean
+    public SecurityContextRepository securityContextRepository() {
+        return new AppSensorSecurityContextRepository();
+    }
+}
