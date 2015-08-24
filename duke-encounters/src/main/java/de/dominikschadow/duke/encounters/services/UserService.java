9db1285bd87265112306e0776b983736853ab74c
Dominik Schadow <dominikschadow@googlemail.com>
2015-08-24 20:41:59 +0200

    Added UserService to create a new user

---
+++ duke-encounters/src/main/java/de/dominikschadow/duke/encounters/services/UserService.java
@@ -0,0 +1,14 @@
+package de.dominikschadow.duke.encounters.services;
+
+import de.dominikschadow.duke.encounters.domain.User;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Dominik Schadow
+ */
+@Service
+public class UserService {
+    public User createUser(User register) {
+        return null;
+    }
+}
