210e20681ee1d7015819b82462893df978ef5946
Dominik Schadow <dominikschadow@googlemail.com>
2015-08-24 20:41:43 +0200

    Added post method to create a new user

--- duke-encounters/src/main/java/de/dominikschadow/duke/encounters/controllers/SessionController.java
+++ duke-encounters/src/main/java/de/dominikschadow/duke/encounters/controllers/SessionController.java
@@ -1,11 +1,16 @@
 package de.dominikschadow.duke.encounters.controllers;
 
 import de.dominikschadow.duke.encounters.domain.User;
+import de.dominikschadow.duke.encounters.services.UserService;
+import de.dominikschadow.duke.encounters.services.ValidationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /**
  * Controller for all session related requests (login, logout, registration).
@@ -14,6 +19,15 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
  */
 @Controller
 public class SessionController {
+    private ValidationService validationService;
+    private UserService userService;
+
+    @Autowired
+    public SessionController(ValidationService validationService, UserService userService) {
+        this.validationService = validationService;
+        this.userService = userService;
+    }
+
     /**
      * Shows the login page.
      *
@@ -40,11 +54,27 @@ public class SessionController {
      * Shows the registration page.
      *
      * @param model The model attribute container
-     * @return Registration URL
+     * @return Register URL
      */
-    @RequestMapping(value = "register", method = GET)
+    @RequestMapping(value = "/register", method = GET)
     public String register(Model model) {
         model.addAttribute("user", new User());
         return "register";
+    }
+
+    /**
+     * Creates the new user and stored it in the database.
+     *
+     * @param model The model attribute container
+     * @return Login URL
+     */
+    @RequestMapping(value = "/register", method = POST)
+    public String register(@ModelAttribute(value = "user") User register, Model model) {
+        validationService.validateUser(register);
+        // TODO react on validation error
+        User user = userService.createUser(register);
+        model.addAttribute("user", user);
+
+        return "login";
     }
 }
