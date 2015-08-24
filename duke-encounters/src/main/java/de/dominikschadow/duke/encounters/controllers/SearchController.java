c2827bd81bbdbd0b453a8b392209a80abf5aa0ea
Dominik Schadow <dominikschadow@googlemail.com>
2015-08-24 19:54:58 +0200

    Added SearchController

---
+++ duke-encounters/src/main/java/de/dominikschadow/duke/encounters/controllers/SearchController.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2015 Dominik Schadow, dominikschadow@gmail.com
+ *
+ * This file is part of the Application Intrusion Detection project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.dominikschadow.duke.encounters.controllers;
+
+import de.dominikschadow.duke.encounters.domain.SearchFilter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+/**
+ * Controller for all search related requests.
+ *
+ * @author Dominik Schadow
+ */
+@Controller
+public class SearchController {
+    /**
+     * Shows the search form.
+     *
+     * @param model The model attribute container
+     * @return Search URL
+     */
+    @RequestMapping(value = "search", method = GET)
+    public String searchEncounters(Model model) {
+        model.addAttribute("searchFilter", new SearchFilter());
+
+        return "search";
+    }
+}
