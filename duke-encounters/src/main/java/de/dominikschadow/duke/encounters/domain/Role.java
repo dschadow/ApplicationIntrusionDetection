f88efda22d0afa19ab071cd802c908052d5d67ef
Dominik Schadow <dominikschadow@googlemail.com>
2015-08-30 12:01:26 +0200

    Temp structure for roles

---
+++ duke-encounters/src/main/java/de/dominikschadow/duke/encounters/domain/Role.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2015 Dominik Schadow, dominikschadow@gmail.com
+ *
+ * This file is part of the Application Intrusion Detection project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.dominikschadow.duke.encounters.domain;
+
+import javax.persistence.*;
+
+/**
+ * Holds a role in the application.
+ *
+ * @author Dominik Schadow
+ */
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String rolename;
+    private String role;
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getRolename() {
+        return rolename;
+    }
+
+    public void setRolename(String rolename) {
+        this.rolename = rolename;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
