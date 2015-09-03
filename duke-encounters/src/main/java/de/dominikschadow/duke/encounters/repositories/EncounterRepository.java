c2d9bbcb0bc701005ab612dcf062bf10cfe7eca3
Dominik Schadow <dominikschadow@googlemail.com>
2015-09-03 22:36:34 +0200

    Query my encounters

--- duke-encounters/src/main/java/de/dominikschadow/duke/encounters/repositories/EncounterRepository.java
+++ duke-encounters/src/main/java/de/dominikschadow/duke/encounters/repositories/EncounterRepository.java
@@ -22,10 +22,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface EncounterRepository extends JpaRepository<Encounter, Long>, JpaSpecificationExecutor {
     @Query(value="select e from Encounter e")
     List<Encounter> findWithPageable(Pageable latestTen);
+
+    @Query(value="select e from Encounter e, User u where e.user = u.id and u.username = :username")
+    List<Encounter> findAllByUsername(@Param("username")String username);
 }
