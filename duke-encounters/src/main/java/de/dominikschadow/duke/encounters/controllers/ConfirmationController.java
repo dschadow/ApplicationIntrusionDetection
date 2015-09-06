c659475579f83c282e473fc0554c466c1d19751d
Dominik Schadow <dominikschadow@googlemail.com>
2015-09-06 21:15:31 +0200

    Added controller to handle confirmation related requests

---
+++ duke-encounters/src/main/java/de/dominikschadow/duke/encounters/controllers/ConfirmationController.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2015 Dominik Schadow, dominikschadow@gmail.com
+ *
+ * This file is part of the Application Intrusion Detection project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.dominikschadow.duke.encounters.controllers;
+
+import de.dominikschadow.duke.encounters.domain.Encounter;
+import de.dominikschadow.duke.encounters.services.EncounterService;
+import de.dominikschadow.duke.encounters.services.ValidationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+/**
+ * Controller to handle all encounter confirmation related requests.
+ */
+@Controller
+public class ConfirmationController {
+    private EncounterService encounterService;
+    private ValidationService validationService;
+
+    @Autowired
+    public ConfirmationController(EncounterService encounterService, ValidationService validationService) {
+        this.encounterService = encounterService;
+        this.validationService = validationService;
+    }
+
+    @RequestMapping(value = "/confirmations/revoke", method = POST)
+    public String revokeConfirmation(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+
+        List<Encounter> encounters = encounterService.getEncountersByUsername(username);
+        model.addAttribute("encounters", encounters);
+
+        return "user/account";
+    }
+}
