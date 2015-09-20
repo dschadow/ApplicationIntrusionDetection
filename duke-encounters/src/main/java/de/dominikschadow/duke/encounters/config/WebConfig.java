07facfeabc5b7666e4be677f2be1b7ac8a6bbcb1
Dominik Schadow <dominikschadow@googlemail.com>
2015-09-20 22:50:48 +0200

    Added config file to scan OWASP AppSensor components

---
+++ duke-encounters/src/main/java/de/dominikschadow/duke/encounters/config/WebConfig.java
@@ -0,0 +1,12 @@
+package de.dominikschadow.duke.encounters.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Dominik Schadow
+ */
+@Configuration
+@ComponentScan(basePackages = "org.owasp.appsensor")
+public class WebConfig {
+}
