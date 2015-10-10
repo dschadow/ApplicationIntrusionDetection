63fd46a82b802aa77702d610f2913f7a1b9b75cc
Dominik Schadow <dominikschadow@googlemail.com>
2015-10-10 20:53:05 +0200

    Externalized constants

---
+++ duke-encounters/src/main/java/de/dominikschadow/duke/encounters/Constants.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2015 Dominik Schadow, dominikschadow@gmail.com
+ *
+ * This file is part of the Application Intrusion Detection project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.dominikschadow.duke.encounters;
+
+/**
+ * Project wide constants. Utility class with hidden constructor to avoid instantiation.
+ *
+ * @author Dominik Schadow
+ */
+public class Constants {
+    private Constants() {
+    }
+
+    public static final String XSS_ERROR_MESSAGE = "This application is XSS bulletproof!";
+    public static final String SQLI_ERROR_MESSAGE = "This application is SQL Injection bulletproof!";
+    public static final String XSS_ERROR_CODE = "xss.attempt";
+    public static final String SQLI_ERROR_CODE = "sqli.attempt";
+    public static final String ATTACK_ERROR_CODE = "attack.attempt";
+}
