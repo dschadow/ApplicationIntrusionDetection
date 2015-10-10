2e9fe20edb6d5f2db8a538271825288cc3f5d6b9
Dominik Schadow <dominikschadow@googlemail.com>
2015-10-10 20:56:50 +0200

    Added XSS and SQLi checks for all register user values

--- duke-encounters/src/main/java/de/dominikschadow/duke/encounters/validators/DukeEncountersUserValidator.java
+++ duke-encounters/src/main/java/de/dominikschadow/duke/encounters/validators/DukeEncountersUserValidator.java
@@ -17,12 +17,16 @@
  */
 package de.dominikschadow.duke.encounters.validators;
 
+import com.google.common.base.Strings;
+import de.dominikschadow.duke.encounters.Constants;
 import de.dominikschadow.duke.encounters.domain.DukeEncountersUser;
+import de.dominikschadow.duke.encounters.services.SecurityValidationService;
+import de.dominikschadow.duke.encounters.services.UserService;
 import org.owasp.appsensor.core.AppSensorClient;
+import org.owasp.appsensor.core.DetectionPoint;
 import org.owasp.appsensor.core.DetectionSystem;
+import org.owasp.appsensor.core.Event;
 import org.owasp.appsensor.core.event.EventManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -36,26 +40,22 @@ import javax.inject.Named;
  */
 @Named
 public class DukeEncountersUserValidator implements Validator {
-    private static final Logger LOGGER = LoggerFactory.getLogger(DukeEncountersUserValidator.class);
-
     private SpringValidatorAdapter validator;
-    private DetectionSystem detectionSystem;
 
     @Autowired
     private javax.validation.Validator jsr303Validator;
-
     @Autowired
     private AppSensorClient appSensorClient;
-
     @Autowired
     private EventManager ids;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private SecurityValidationService securityValidationService;
 
     @PostConstruct
     public void init() {
         validator = new SpringValidatorAdapter(jsr303Validator);
-        detectionSystem = new DetectionSystem(
-                appSensorClient.getConfiguration().getServerConnection()
-                        .getClientApplicationIdentificationHeaderValue());
     }
 
     @Override
@@ -69,5 +69,59 @@ public class DukeEncountersUserValidator implements Validator {
 
         DukeEncountersUser user = (DukeEncountersUser) target;
 
+        if (!Strings.isNullOrEmpty(user.getFirstname())) {
+            if (securityValidationService.hasXssPayload(user.getFirstname())) {
+                fireXssEvent();
+                errors.rejectValue("firstname", Constants.XSS_ERROR_CODE, Constants.XSS_ERROR_MESSAGE);
+            } else if (securityValidationService.hasSqlIPayload(user.getFirstname())) {
+                fireSqlIEvent();
+                errors.rejectValue("firstname", Constants.SQLI_ERROR_CODE, Constants.SQLI_ERROR_MESSAGE);
+            }
+        }
+
+        if (!Strings.isNullOrEmpty(user.getLastname())) {
+            if (securityValidationService.hasXssPayload(user.getLastname())) {
+                fireXssEvent();
+                errors.rejectValue("lastname", Constants.XSS_ERROR_CODE, Constants.XSS_ERROR_MESSAGE);
+            } else if (securityValidationService.hasSqlIPayload(user.getLastname())) {
+                fireSqlIEvent();
+                errors.rejectValue("lastname", Constants.SQLI_ERROR_CODE, Constants.SQLI_ERROR_MESSAGE);
+            }
+        }
+
+        if (!Strings.isNullOrEmpty(user.getUsername())) {
+            if (securityValidationService.hasXssPayload(user.getUsername())) {
+                fireXssEvent();
+                errors.rejectValue("username", Constants.XSS_ERROR_CODE, Constants.XSS_ERROR_MESSAGE);
+            } else if (securityValidationService.hasSqlIPayload(user.getUsername())) {
+                fireSqlIEvent();
+                errors.rejectValue("username", Constants.SQLI_ERROR_CODE, Constants.SQLI_ERROR_MESSAGE);
+            }
+        }
+
+        if (!Strings.isNullOrEmpty(user.getEmail())) {
+            if (securityValidationService.hasXssPayload(user.getEmail())) {
+                fireXssEvent();
+                errors.rejectValue("email", Constants.XSS_ERROR_CODE, Constants.XSS_ERROR_MESSAGE);
+            } else if (securityValidationService.hasSqlIPayload(user.getEmail())) {
+                fireSqlIEvent();
+                errors.rejectValue("email", Constants.SQLI_ERROR_CODE, Constants.SQLI_ERROR_MESSAGE);
+            }
+        }
+    }
+
+    private void fireXssEvent() {
+        DetectionPoint detectionPoint = new DetectionPoint(DetectionPoint.Category.INPUT_VALIDATION, "IE1");
+        ids.addEvent(new Event(userService.getUser(), detectionPoint, getDetectionSystem()));
+    }
+
+    private void fireSqlIEvent() {
+        DetectionPoint detectionPoint = new DetectionPoint(DetectionPoint.Category.COMMAND_INJECTION, "CIE1");
+        ids.addEvent(new Event(userService.getUser(), detectionPoint, getDetectionSystem()));
+    }
+
+    private DetectionSystem getDetectionSystem() {
+        return new DetectionSystem(appSensorClient.getConfiguration().getServerConnection()
+                .getClientApplicationIdentificationHeaderValue());
     }
 }
