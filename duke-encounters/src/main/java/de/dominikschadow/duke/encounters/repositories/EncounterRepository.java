1a1baf08cefcdce1c92d2974b091b4f1e2c47e5d
Dominik Schadow <dominikschadow@googlemail.com>
2015-11-03 16:13:04 +0100

    order by for encounters

--- duke-encounters/src/main/java/de/dominikschadow/duke/encounters/repositories/EncounterRepository.java
+++ duke-encounters/src/main/java/de/dominikschadow/duke/encounters/repositories/EncounterRepository.java
@@ -35,7 +35,8 @@ public interface EncounterRepository extends JpaRepository<Encounter, Long>, Jpa
     @Query(value = "select e from Encounter e")
     List<Encounter> findWithPageable(Pageable latestTen);
 
-    @Query(value = "select e from Encounter e, DukeEncountersUser u where e.user = u.id and u.username = :username")
+    @Query(value = "select e from Encounter e, DukeEncountersUser u where e.user = u.id and u.username = :username " +
+            "order by e.date desc ")
     List<Encounter> findAllByUsername(@Param("username") String username);
 
     @Query(value = "select e from Encounter e, DukeEncountersUser u where e.user = u.id and u.username = :username " +
