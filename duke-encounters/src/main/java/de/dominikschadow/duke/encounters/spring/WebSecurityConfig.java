1490f9d5640b4c8c25116ff947dd16e904a583f8
Dominik Schadow <dominikschadow@googlemail.com>
2015-11-18 19:40:07 +0100

    Replaced deprecated annotation

--- duke-encounters/src/main/java/de/dominikschadow/duke/encounters/spring/WebSecurityConfig.java
+++ duke-encounters/src/main/java/de/dominikschadow/duke/encounters/spring/WebSecurityConfig.java
@@ -24,8 +24,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.context.SecurityContextRepository;
@@ -38,7 +38,7 @@ import javax.sql.DataSource;
  * @author Dominik Schadow
  */
 @Configuration
-@EnableWebMvcSecurity
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private SecurityContextRepository securityContextRepository;
