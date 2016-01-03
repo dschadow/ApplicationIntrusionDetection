00a4389fc305eef99b8535bdffe8891deea89617
Dominik Schadow <dominikschadow@googlemail.com>
2016-01-03 14:52:54 +0100

    Moved integration tests to separate package

--- duke-encounters/src/integration-test/java/de/dominikschadow/duke/encounters/ServerWebTests.java
+++ duke-encounters/src/integration-test/java/de/dominikschadow/duke/encounters/ServerWebTests.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.dominikschadow.duke.encounters.selenium;
+package de.dominikschadow.duke.encounters;
 
 import de.dominikschadow.duke.encounters.DukeEncountersApplication;
 import org.junit.AfterClass;
