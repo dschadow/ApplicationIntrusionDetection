e228635e83853e9e3f8ba9219289488c3eac0a2f
Dominik Schadow <dominikschadow@googlemail.com>
2016-03-23 19:51:55 +0100

    Switched to Spring Security annotations

--- duke-encounters/src/test/java/de/dominikschadow/duke/encounters/controllers/AccountControllerTests.java
+++ duke-encounters/src/test/java/de/dominikschadow/duke/encounters/controllers/AccountControllerTests.java
@@ -23,17 +23,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Tests the [@link AccountController} class.
@@ -55,12 +54,14 @@ public class AccountControllerTests {
     }
 
     @Test
+    @WithMockUser(username = "arthur@dent.com", password = "arthur@dent.com", roles = "DUMMY")
     public void verifyAccountAuthorizeNOK() throws Exception {
-        mockMvc.perform(get("/account").with(user("dummy").roles("TEST"))).andExpect(status().isForbidden());
+        mockMvc.perform(get("/account")).andExpect(status().isForbidden());
     }
 
     @Test
+    @WithMockUser(username = "arthur@dent.com", password = "arthur@dent.com", roles = "USER")
     public void verifyAccountAuthorizeOK() throws Exception {
-        mockMvc.perform(get("/account").with(user("arthur@dent.com").password("arthur@dent.com").roles("USER"))).andExpect(status().isOk());
+        mockMvc.perform(get("/account")).andExpect(status().isOk());
     }
 }
