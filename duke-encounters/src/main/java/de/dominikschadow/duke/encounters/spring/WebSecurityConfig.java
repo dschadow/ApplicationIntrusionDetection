9861d142975de9dd2a25b51ba40f3268e2d5026c
Dominik Schadow <dominikschadow@googlemail.com>
2016-03-23 19:38:07 +0100

    enabled PreAuthorize

--- duke-encounters/src/main/java/de/dominikschadow/duke/encounters/spring/WebSecurityConfig.java
+++ duke-encounters/src/main/java/de/dominikschadow/duke/encounters/spring/WebSecurityConfig.java
@@ -22,6 +22,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -40,6 +41,7 @@ import javax.sql.DataSource;
  */
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private SecurityContextRepository securityContextRepository;
