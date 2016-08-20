49c517e039d8af9ea2157d7a8ed909ba59d2ff65
Dominik Schadow <dominikschadow@googlemail.com>
2016-08-20 17:48:44 +0200

    Refactoring

--- duke-encounters/src/test/java/de/dominikschadow/duke/encounters/controller/AccountControllerTest.java
+++ duke-encounters/src/test/java/de/dominikschadow/duke/encounters/controller/AccountControllerTest.java
@@ -17,20 +17,19 @@
  */
 package de.dominikschadow.duke.encounters.controller;
 
-import de.dominikschadow.duke.encounters.DukeEncountersApplication;
-import org.junit.Before;
+import de.dominikschadow.duke.encounters.services.ConfirmationService;
+import de.dominikschadow.duke.encounters.services.EncounterService;
+import de.dominikschadow.duke.encounters.services.UserService;
+import de.dominikschadow.duke.encounters.validators.DukeEncountersUserValidator;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -40,28 +39,29 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * @author Dominik Schadow
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = DukeEncountersApplication.class)
-@WebAppConfiguration
-public class AccountControllerTests {
-    @Autowired
-    private WebApplicationContext context;
+@WebMvcTest(AccountController.class)
+public class AccountControllerTest {
+    @MockBean
+    private EncounterService encounterService;
+    @MockBean
+    private UserService userService;
+    @MockBean
+    private ConfirmationService confirmationService;
+    @MockBean
+    private DukeEncountersUserValidator dukeEncountersUserValidator;
 
+    @Autowired
     private MockMvc mvc;
 
-    @Before
-    public void setup() {
-        mvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+    @Test
+    @WithMockUser(username = "arthur@dent.com", password = "arthur@dent.com", roles = "USER")
+    public void verifyAccountAuthorizeOK() throws Exception {
+        mvc.perform(get("/account")).andExpect(status().isOk());
     }
 
     @Test
     @WithMockUser(username = "arthur@dent.com", password = "arthur@dent.com", roles = "DUMMY")
     public void verifyAccountAuthorizeNOK() throws Exception {
         mvc.perform(get("/account")).andExpect(status().isForbidden());
-    }
-
-    @Test
-    @WithMockUser(username = "arthur@dent.com", password = "arthur@dent.com", roles = "USER")
-    public void verifyAccountAuthorizeOK() throws Exception {
-        mvc.perform(get("/account")).andExpect(status().isOk());
     }
 }
