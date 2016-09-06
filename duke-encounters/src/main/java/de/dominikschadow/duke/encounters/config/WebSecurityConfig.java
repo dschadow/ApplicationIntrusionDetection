726a5dfd852c214d1af280c95e44c9e9cb538c15
Dominik Schadow <dominikschadow@googlemail.com>
2016-09-06 16:13:48 +0200

    Removed SSO integration

--- duke-encounters/src/main/java/de/dominikschadow/duke/encounters/config/WebSecurityConfig.java
+++ duke-encounters/src/main/java/de/dominikschadow/duke/encounters/config/WebSecurityConfig.java
@@ -19,13 +19,8 @@ package de.dominikschadow.duke.encounters.config;
 
 import org.owasp.appsensor.integration.springsecurity.context.AppSensorSecurityContextRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
-import org.springframework.boot.context.embedded.FilterRegistrationBean;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -34,24 +29,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
-import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
-import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.security.web.header.writers.StaticHeadersWriter;
-import org.springframework.web.filter.CompositeFilter;
 
-import javax.servlet.Filter;
 import javax.sql.DataSource;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Spring Security configuration file.
@@ -61,18 +43,12 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-@EnableOAuth2Client
-@EnableAuthorizationServer
-@Order(6)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private SecurityContextRepository securityContextRepository;
 
     @Autowired
     private DataSource dataSource;
-
-    @Autowired
-    private OAuth2ClientContext oauth2ClientContext;
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
@@ -100,44 +76,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .securityContextRepository(securityContextRepository)
             .and()
                 .headers()
-                .addHeaderWriter(new StaticHeadersWriter("Content-Security-Policy", "default-src 'self'"))
-            .and()
-			    .addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
+                .addHeaderWriter(new StaticHeadersWriter("Content-Security-Policy", "default-src 'self'"));
         // @formatter:on
-    }
-
-    @Bean
-    public FilterRegistrationBean oauth2ClientFilterRegistration(final OAuth2ClientContextFilter filter) {
-        FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(filter);
-        registration.setOrder(-100);
-        return registration;
-    }
-
-    @Bean
-    @ConfigurationProperties("github")
-    ClientResources github() {
-        return new ClientResources();
-    }
-
-    private Filter ssoFilter() {
-        CompositeFilter filter = new CompositeFilter();
-        List<Filter> filters = new ArrayList<>();
-        filters.add(ssoFilter(github(), "/login/github"));
-        filter.setFilters(filters);
-        return filter;
-    }
-
-    private Filter ssoFilter(final ClientResources client, final String path) {
-        OAuth2ClientAuthenticationProcessingFilter oAuth2ClientAuthenticationFilter =
-                new OAuth2ClientAuthenticationProcessingFilter(path);
-        OAuth2RestTemplate oAuth2RestTemplate = new OAuth2RestTemplate(client.getClient(), oauth2ClientContext);
-        oAuth2ClientAuthenticationFilter.setRestTemplate(oAuth2RestTemplate);
-        UserInfoTokenServices tokenServices = new UserInfoTokenServices(
-                client.getResource().getUserInfoUri(), client.getClient().getClientId());
-        tokenServices.setRestTemplate(oAuth2RestTemplate);
-        oAuth2ClientAuthenticationFilter.setTokenServices(tokenServices);
-        return oAuth2ClientAuthenticationFilter;
     }
 
     /**
@@ -173,18 +113,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public SecurityContextRepository securityContextRepository() {
         return new AppSensorSecurityContextRepository();
-    }
-
-    static class ClientResources {
-        private OAuth2ProtectedResourceDetails client = new AuthorizationCodeResourceDetails();
-        private ResourceServerProperties resource = new ResourceServerProperties();
-
-        public OAuth2ProtectedResourceDetails getClient() {
-            return client;
-        }
-
-        public ResourceServerProperties getResource() {
-            return resource;
-        }
     }
 }
